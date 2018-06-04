6b40565c485d7bf871d797e0897de3eb1060997c
Tom <37577470+footisgood@users.noreply.github.com>
2018-06-04 16:44:25 +0800

    Update ppt2jpgwithremarks.py

--- ppt2jpgwithremarks.py
+++ ppt2jpgwithremarks.py
@@ -1,7 +1,10 @@
+#pyinstaller -F -w -c ppt2jpgwithremarks.py
+
 import time
 import pyautogui
 
 n=int(input('Please input the total page of the PPT:'))
+time_wait=int(input('Please input the wait time:'))
 print ('\n')
 print ('Now you have 5 seconds to open the PPT and play...')
 time.sleep(5)
@@ -10,4 +13,4 @@ for i in range(n): # Turn page
    pyautogui.keyDown('right')
    pyautogui.keyUp('right') # Take and save a screenshot
    pyautogui.screenshot('images/page_%d.jpg' % i)
-   time.sleep(1)
+   time.sleep(time_wait)
